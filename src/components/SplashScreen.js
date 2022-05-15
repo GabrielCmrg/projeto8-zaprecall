3c3231e472a5940fcff65261c7d928f877c7eae9
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-05-15 19:01:09 -0300

    initial screen done

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
+import logo from "../assets/images/logo.png";
+
 export default function SplashScreen() {
     return (
-        <></>
+        <div className="splash">
+            <img src={logo} alt="logo" width="136" height="161" />
+            <h1>ZapRecall</h1>
+            <Link to="/flashcards">
+                <button>Iniciar Recall!</button>            
+            </Link>
+        </div>
     )
 }
