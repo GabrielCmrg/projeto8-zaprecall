c9d40b52f53a1341fae87458e7effafb4828bb88
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-05-16 13:40:25 -0300

    finish message done

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,3 +1,30 @@
+import party from "../assets/images/party.png";
+import sad from "../assets/images/sad.png";
+
+function FinishMessage({answerList}) {
+    let pic;
+    let title;
+    let message;
+    if (answerList.includes("incorrect")) {
+        pic = sad;
+        title = "Putz...";
+        message = "Ainda faltam alguns... Mas não desanime!";
+    } else {
+        pic = party;
+        title = "Parabéns!";
+        message = "Você não esqueceu de nenhum flashcard!";
+    }
+    return (
+        <>
+            <div className="title">
+                <img src={pic} alt="emoji" width="23" height="23" />
+                <span>{title}</span>
+            </div>
+            <p>{message}</p>
+        </>
+    )
+}
+
 export default function Bottom({count, answerList}) {
     const iconList = [];
     answerList.forEach(result => {
@@ -12,6 +39,7 @@ export default function Bottom({count, answerList}) {
 
     return (
         <div className="bottom">
+            {count === 4? <FinishMessage answerList={answerList}/>: <></>}
             <span className="bottom-message">{count.toString() + "/4 CONCLUÍDOS"}</span>
             <div className="answer-list">
                 { iconList.map(icon => <ion-icon name={icon.name} style={{color: icon.color}}></ion-icon>)}
